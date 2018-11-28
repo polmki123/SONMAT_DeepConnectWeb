4424e2c284181e5632d6678707be4ed9a623bca3
hhjung1202 <hhjung1202@naver.com>
2018-11-28 22:10:53 +0900

    test url

--- workspace/routes/index.js
+++ workspace/routes/index.js
@@ -6,9 +6,26 @@ var fontService = require('../service/font');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	res.render('index', { title: 'Express', title2: 'Condition', is_check: true});
+});
+
+router.get('/1', function(req, res, next) {
+	fontService.download_example()
+	res.render('index', { title: 'Express', title2: 'Condition', is_check: true});
+});
+router.get('/2', function(req, res, next) {
 	fontService.python_example()
 	res.render('index', { title: 'Express', title2: 'Condition', is_check: true});
 });
+router.get('/3', function(req, res, next) {
+	fontService.upload_WS_example()
+	res.render('index', { title: 'Express', title2: 'Condition', is_check: true});
+});
+router.get('/4', function(req, res, next) {
+	fontService.upload_WAS_example()
+	res.render('index', { title: 'Express', title2: 'Condition', is_check: true});
+});
+
 router.get('/false', function(req, res, next) {
 	res.render('index', { title: 'Express', title2: 'Condition', is_check: false});
 });
