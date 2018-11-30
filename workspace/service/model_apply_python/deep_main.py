6b17b23e4166969152ab2385c6224f21c14cfacb
ksj01401 <ksj01401@gmail.com>
2018-11-30 21:27:11 +0900

    예시용 폰트 업로드

--- workspace/service/model_apply_python/deep_main.py
+++ workspace/service/model_apply_python/deep_main.py
@@ -4,5 +4,8 @@ import os
 # input image file path is 
 # sys.argv[1]
 
-print(sys.argv[1])
-return sys.argv[1]
+ttf_files = ['/home/deep_user/deploy/sonmat/webapp/workspace/repository/117/1.ttf',
+'/home/deep_user/deploy/sonmat/webapp/workspace/repository/117/2.ttf',
+'/home/deep_user/deploy/sonmat/webapp/workspace/repository/117/3.ttf']
+
+print(ttf_files)
