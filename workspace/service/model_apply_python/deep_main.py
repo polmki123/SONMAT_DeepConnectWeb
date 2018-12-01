da77fb86c4576bf0497990236973863bf05949bc
churhong <ok6403@gmail.com>
2018-12-01 12:34:48 +0900

    change python file

--- workspace/service/model_apply_python/deep_main.py
+++ workspace/service/model_apply_python/deep_main.py
@@ -1,12 +1,86 @@
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+#from . import utils
+import utils
 import sys
 import os
+import time
+import glob
+from PIL import Image
+import numpy as np
+import PIL.ImageOps
+from model import *
 
-# input image file path is 
-# sys.argv[1]
 
-ttf_files = ['/home/deep_user/deploy/sonmat/webapp/workspace/repository/117/1.ttf',
-'/home/deep_user/deploy/sonmat/webapp/workspace/repository/117/2.ttf',
-'/home/deep_user/deploy/sonmat/webapp/workspace/repository/117/3.ttf']
 
-for ttf_file in ttf_files:
-    print(ttf_file)
+def input_Deepmodel_image(inputimagedir):
+	frame_dir = '../frame_label/'
+	frame_paths = glob.glob(os.path.join(frame_dir, '*.jpg'))
+	frame_names = os.listdir(frame_dir)
+	input_data = list()
+	for frame in frame_paths:
+		frame_image = np.array(Image.open(frame)).reshape(1, 64, 64)
+		input_image = np.array(Image.open(inputimagedir))
+		input_image = np.array(np.split(input_image, 8, axis=1))  # 8*64*64
+		Concat_data = np.append(input_image, frame_image, axis=0)
+		if ((9, 64, 64) == Concat_data.shape):
+			input_data.append(Concat_data)
+
+	return input_data, frame_names
+
+
+
+def main(inputimagedir = '../Deep_model/test1.jpg', model_dir):
+	start_time = time.time()
+	input_data, output_name = utils.input_Deepmodel_image(inputimagedir)
+	utils.default_model_dir = model_dir
+
+	model = ResNet()
+	model = nn.DataParallel(model)
+	checkpoint = utils.load_checkpoint(model_dir+str(12))
+	if not checkpoint:
+		pass
+	else:
+		model.load_state_dict(checkpoint['state_dict'])
+		model.eval()
+		number = 0
+		for count in range(len(input_data)):
+			i = input_data[count]
+			number = number + 1
+			i = np.array(i)
+			i = i.reshape(1,9,64,64)
+			input = torch.from_numpy(i)
+			# input = Variable(input)
+			input = input.type(torch.FloatTensor)
+			input = utils.normalize_image(input)
+			output = model(input)
+			output = Variable(output[1]).data.cpu().numpy()
+			# print(output.shape)
+			output = output.reshape(64,64)
+			output = utils.renormalize_image(output)
+			output = utils.normalize_function(output)
+			img = Image.fromarray(output.astype('uint8'), 'L')
+			img = PIL.ImageOps.invert(img)
+			img.save('../save_image/' + output_name[count][:-4] + '.png')
+
+        
+	now = time.gmtime(time.time() - start_time)
+	print('{} hours {} mins {} secs for data'.format(now.tm_hour, now.tm_min, now.tm_sec))
+	
+if __name__ == "__main__":
+	inputimagedir = sys.argv[1]
+	model_dir = '../model/'
+	main(inputimagedir, model_dir)
+
+# # input image file path is 
+# # sys.argv[1]
+
+# ttf_files = ['/home/deep_user/deploy/sonmat/webapp/workspace/repository/117/1.ttf',
+# '/home/deep_user/deploy/sonmat/webapp/workspace/repository/117/2.ttf',
+# '/home/deep_user/deploy/sonmat/webapp/workspace/repository/117/3.ttf']
+
+# for ttf_file in ttf_files:
+#     print(ttf_file)
