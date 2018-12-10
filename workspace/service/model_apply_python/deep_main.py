ff7fecea6f07181a4f8b4ef78efb81661dd37d95
churhong <ok6403@gmail.com>
2018-12-11 01:21:05 +0900

    test

--- workspace/service/model_apply_python/deep_main.py
+++ workspace/service/model_apply_python/deep_main.py
@@ -98,7 +98,7 @@ def make_image_process(input_data, model, output_name, save_image_dir):
         img = cv2.erode(img, kernel, iterations=1)
         img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
         img = Image.fromarray(img, 'L')
-        img = img.point(lambda p: p > 80 and 255)
+        img = img.point(lambda p: p > 190 and 255)
         img = img.filter(ImageFilter.SHARPEN)
         if not os.path.exists(save_image_dir):
             os.makedirs(save_image_dir)
